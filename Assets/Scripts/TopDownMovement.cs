e4f75758c9e8e3ce11f5c299ceeae840e93cf4d0
Hixxson <70636349+Hixxson@users.noreply.github.com>
2023-12-06 16:45:57 +0000

    Update2.1
    hi

--- Assets/Scripts/TopDownMovement.cs
+++ Assets/Scripts/TopDownMovement.cs
@@ -1,25 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TopDownMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb2d;
-    private Vector2 moveInput;
+
+    public RuntimeAnimatorController idleController;
+    public RuntimeAnimatorController movementController;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator.runtimeAnimatorController = idleController; // Start with the idle controller
     }
 
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
 
+        Vector2 moveInput = new Vector2(moveHorizontal, moveVertical);
         moveInput.Normalize();
 
         rb2d.velocity = moveInput * moveSpeed;
+
+        // Switch between idle and movement Animator Controllers based on movement
+        if (moveInput.magnitude > 0.1f)
+        {
+            // Switch to the movement controller
+            if (animator.runtimeAnimatorController != movementController)
+                animator.runtimeAnimatorController = movementController;
+
+            // Flip the sprite based on movement direction
+            FlipSprite(moveInput.x);
+        }
+        else
+        {
+            // Switch to the idle controller
+            if (animator.runtimeAnimatorController != idleController)
+                animator.runtimeAnimatorController = idleController;
+        }
+    }
+
+    void FlipSprite(float horizontalInput)
+    {
+        if (horizontalInput < 0)
+            spriteRenderer.flipX = true; // Flip the sprite when moving left
+        else if (horizontalInput > 0)
+            spriteRenderer.flipX = false; // Unflip the sprite when moving right
     }
 }
+
+
+
